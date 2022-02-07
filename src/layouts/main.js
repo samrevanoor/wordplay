68740442b9656f7159ab6f7f1302a38c95ea88c0
Sam Revanoor <sam@trint.com>
2022-02-07 18:06:08 +0000

    adds link to heroku site to samrevanoor domain

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -6,7 +6,16 @@ const Main = () => {
     <>
       <header className="header">W O R D L E Z</header>
       <div className="content">
-        <GameBoard />
+        {/* <GameBoard /> */}
+        <span style={{ textAlign: "center" }}>
+          to play, go to
+          <br />
+          <a href="wordlez.samrevanoor.com">wordlez.samrevanoor.com</a>
+        </span>
+      </div>
+      <br />
+      <div style={{ display: "flex", justifyContent: " center" }}>
+        <img src="favicon.ico" alt="flower" width="80px" />
       </div>
       <footer className="footer">
         with love, <br />
